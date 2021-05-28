0d94099f74b98eaa1b0bbb575db3a182278c228e
mariiamhossam <65025096+mariiamhossam@users.noreply.github.com>
2021-05-29 01:44:45 +0200

    Update Donor.h

--- Blood Bank System/Donor.h
+++ Blood Bank System/Donor.h
@@ -1,30 +1,28 @@
+#pragma once
 #include <string>
-#include"User.h"
-#include<vector>
 using namespace std;
-
-
 vector <Donor> donorsList;
-
-
 struct Date
 {
 	int day, month, year;
 };
 
-class Donor:public User
+class Donor
 {
 public:
-
-	string   Blood_type, Disease;
-
+	string Name, Email, Password, Blood_type;
+	bool Disease = false;
+	int ID, Age;
 	int Donor_count = 0;
+	char Gender;
 	Date Latest_Donation_Date;
 
 public:
 	Donor();
-	Donor(string, int, char, string, string,string, string, Date);
+	Donor(string, int, char, string, string, string,bool, Date);
 	void Donation_Request(int);
 	void Update_Data(int);
+	void Delete_Account();
 	void Donor_page(int);
+	void Donor_Registeration();
 };
