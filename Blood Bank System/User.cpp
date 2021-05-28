e6b63d1dee0f15e6c96156a3c6f0b154da24488a
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-05-28 21:29:29 +0200

    Update User.cpp

--- Blood Bank System/User.cpp
+++ Blood Bank System/User.cpp
@@ -1,1 +1,32 @@
+
 #include "User.h"
+#include"Admin.h"
+#include<iostream>
+#include<string>
+#include"Donor.h"
+#include"Recipient.h"
+
+using namespace std;
+
+User::User()
+{
+
+}
+
+User::User(int id,string name, int age, char gender, string mail, string pass)
+{
+	ID = id;
+	Name = name;
+	Email = mail;
+	Password = pass;
+	Age = age;
+	Gender = gender;
+}
+void Update_Data(int)
+{
+
+}
+void Delete_Account()
+{
+
+}
