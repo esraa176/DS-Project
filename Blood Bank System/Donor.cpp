f5c234b08f566ee369fd5eee4c41a37d8086ddab
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-05-28 21:27:11 +0200

    Update Donor.cpp

--- Blood Bank System/Donor.cpp
+++ Blood Bank System/Donor.cpp
@@ -10,23 +10,19 @@ Donor::Donor()
 {
 
 }
-Donor::Donor(string name, int age, char gender, string mail, string password, string blood_type, string disease, Date latest_donation_date)
+Donor::Donor(string name, int age, char gender, string mail, string pass,string blood_type, string disease, Date latest_donation_date)
 {
 	ID = Donor_count;
+	//User( ID,  name,  age,  gender,  mail,  pass);
 	Donor_count++;
-	Name = name;
-	Age = age;
-	Gender = gender;
-	Email = mail;
-	Password = password;
 	Blood_type = blood_type;
 	Disease = disease;
 	Latest_Donation_Date = latest_donation_date;
 }
 void Donor::Donor_page(int userIndex)
 {
-	
-	
+
+
 }
 void Donor::Donation_Request(int indx)
 {
