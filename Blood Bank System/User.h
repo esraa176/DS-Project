b59ecff7edba016881e2cffaa0831f5a75b2fbd0
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-05-28 22:26:51 +0200

    Update User.h

--- Blood Bank System/User.h
+++ Blood Bank System/User.h
@@ -1,5 +1,18 @@
-#pragma once
+#include<string>
+#include<iostream>
+
+using namespace std;
 class User
 {
-};
+public:
+	int ID, Age;
+	string Name, Email, Password;
+	char Gender;
 
+public:
+	User(int, string, int, char, string ,string);
+	User();
+
+	void Update_Data(int);
+	void Delete_Account();
+};
