2e170ca2f2c610e8530884e43ca14668e400ffe1
esraa176 <72940480+esraa176@users.noreply.github.com>
2021-05-29 13:51:44 +0200

    Removed all errors

--- Blood Bank System/Admin.cpp
+++ Blood Bank System/Admin.cpp
@@ -1,27 +1,59 @@
-#pragma once
-#include "Donor.h"
+#include"Admin.h"
+#include<iostream>
+#include<string>
 #include <vector>
-#include <string>
+#include"Donor.h"
 using namespace std;
 
-class Admin
+Admin::Admin()
 {
-public:
-	int ID, Age;
-	string Name, Email, Password, code = "Blood Bank";
-	char Gender;
-	int Admin_count = 0;
 
-public:
-	Admin(string, int, char, string, string);
-	Admin();
-	void Admin_page(int, vector <Admin>&);
-	void validateRequests();
-	void addDonors();
-	void Admin_Register(vector <Admin>&);
+}
+Admin::Admin(string name, int age, char gender, string email, string password)
+{
+	Name = name;
+	Age = age;
+	Gender = gender;
+	Email = email;
+	Password = password;
+	ID = Admin_count;
+	Admin_count++;
+}
 
-	//display & validate donor's requests
-	//Insert/Update/Delete the quantity of blood, and its received and expiry dates 
-	//Insert/Update/Delete/Display data of donors 
-	//Insert/Update/Delete/Display data of recipients
-};
+void Admin::Admin_page(int userIndex, vector <Admin>& adminsList)
+{
+
+}
+void Admin::validateRequests()
+{
+
+}
+
+void Admin::addDonors()
+{
+
+}
+
+void Admin::Admin_Register(vector <Admin>& adminsList)
+{
+	cout << "Please enter Admin's code: ";
+	string input_code;
+	cin.ignore();
+	getline(cin, input_code);
+	if (input_code != code)
+	{
+		cout << "\nInvalid code. You can't be an admin.\n";
+		return;
+	}
+	int age;
+	string name, email, pass;
+	char gender;
+	cout << "Please enter your name, age and gender (M/F).\n";
+	cin >> name >> age >> gender;
+	cout << "Please enter your Email and Password.\n";
+	cin >> email >> pass;
+	Admin reg(name, age, gender, email, pass);
+	adminsList.push_back(reg);
+	cout << "\t\t\t\t REGISTERATION SUCCESSFUL! \n\t\t Welcome to Our Blood Bank Management System!\n";
+	Admin_page(adminsList.size() - 1, adminsList);
+}
