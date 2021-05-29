fd19e8e85c48499d46b82fbacba44cf1d2942534
esraa176 <72940480+esraa176@users.noreply.github.com>
2021-05-29 13:51:19 +0200

    Removed all errors

--- Blood Bank System/Admin.cpp
+++ Blood Bank System/Admin.cpp
@@ -1,33 +1,27 @@
-
-#include"Admin.h"
-
-
+#pragma once
+#include "Donor.h"
+#include <vector>
+#include <string>
 using namespace std;
 
-Admin::Admin()
+class Admin
 {
+public:
+	int ID, Age;
+	string Name, Email, Password, code = "Blood Bank";
+	char Gender;
+	int Admin_count = 0;
 
-}
-Admin::Admin(string name, int age, char gender, string email, string password)
-{
-	
-	ID = Admin_count;
-	//User(ID, name, age, gender, email, password);
-	Admin_count++;
-}
+public:
+	Admin(string, int, char, string, string);
+	Admin();
+	void Admin_page(int, vector <Admin>&);
+	void validateRequests();
+	void addDonors();
+	void Admin_Register(vector <Admin>&);
 
-void Admin::Admin_page(int userIndex)
-{
-	
-
-}
-void Admin::validateRequests()
-{
-
-}
-
-void Admin::addDonors()
-{
-
-}
-
+	//display & validate donor's requests
+	//Insert/Update/Delete the quantity of blood, and its received and expiry dates 
+	//Insert/Update/Delete/Display data of donors 
+	//Insert/Update/Delete/Display data of recipients
+};
