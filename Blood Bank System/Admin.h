c6becd96c2d7f8a196ba3a48a7355bd87964fe2a
esraa176 <72940480+esraa176@users.noreply.github.com>
2021-05-31 23:52:29 +0200

    Update Admin.h

--- Blood Bank System/Admin.h
+++ Blood Bank System/Admin.h
@@ -16,14 +16,14 @@ public:
 public:
 	Admin(string, int, char, string, string);
 	Admin();
-	void Admin_page(int , vector <Admin>& , vector <Donor>& , vector <Recipient>& );
+	void Admin_page(int, vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&);
 	void validateRequests();
-	void Admin_Register(vector <Admin>& , vector <Donor>& , vector <Recipient>& );
-	void insertUser(vector <Admin>& , vector <Donor>& , vector <Recipient>& );
+	void Admin_Register(vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&);
+	void insertUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&);
 	void deleteUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&);
 	void updateUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&);
 	void insertBlood();
 	void deleteBlood();
 	void updateBlood();
-
+	void displayData(int, vector <Admin>&, vector <Donor>&, vector <Recipient>&, char);
 };
