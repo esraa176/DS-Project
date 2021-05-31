4d549298bcb509fdff4ee670330c620b2123ff56
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-05-31 14:18:34 +0200

    Update Admin.h

--- Blood Bank System/Admin.h
+++ Blood Bank System/Admin.h
@@ -21,13 +21,9 @@ public:
 	void Admin_Register(vector <Admin>& , vector <Donor>& , vector <Recipient>& );
 	void insertUser(vector <Admin>& , vector <Donor>& , vector <Recipient>& );
 	void deleteUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&);
-	void updateUser(vector <Admin>& );
+	void updateUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&);
 	void insertBlood();
 	void deleteBlood();
 	void updateBlood();
 
-	//display & validate donor's requests
-	//Insert/Update/Delete the quantity of blood, and its received and expiry dates 
-	//Insert/Update/Delete/Display data of donors 
-	//Insert/Update/Delete/Display data of recipients
 };
