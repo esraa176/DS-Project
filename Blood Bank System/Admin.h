a655ac6dfc2207cc60000b48fc55330b7740a71c
esraa176 <72940480+esraa176@users.noreply.github.com>
2021-06-01 23:45:32 +0200

    id problem fixed

--- Blood Bank System/Admin.h
+++ Blood Bank System/Admin.h
@@ -3,11 +3,11 @@
 #include"Recipient.h"
 #include <vector>
 #include <string>
-#include"User.h"
+//#include"User.h"
 #include"Blood.h"
 using namespace std;
 
-class Admin : public User
+class Admin //: public User
 {
 public:
 	int ID, Age;
@@ -16,13 +16,13 @@ public:
 	int Admin_count = 0;
 
 public:
-	Admin(string, int, char, string, string);
+	Admin(string, int, char, string, string, int&);
 	Admin();
-	void Admin_page(int, vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&);
+	void Admin_page(int, vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, int&, int&, int&);
 	void validateRequests(vector <Donor>&, queue <int>&);
-	void Display_requests(vector <Donor>& , queue <int>& );
-	void Admin_Register(vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&);
-	void insertUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&);
+	void Display_requests(vector <Donor>&, queue <int>&);
+	void Admin_Register(vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, int&, int&, int&);
+	void insertUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&, queue<int>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, int&, int&, int&);
 	void deleteUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&);
 	void updateUser(vector <Admin>&, vector <Donor>&, vector <Recipient>&);
 	void insertBlood();
