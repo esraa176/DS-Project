d0d0f454e23d86d68cf031a4a0628216d573787b
MaaiiMohamed <76558259+MaaiiMohamed@users.noreply.github.com>
2021-06-02 08:30:58 +0200

    Update Recipient.h

--- Blood Bank System/Recipient.h
+++ Blood Bank System/Recipient.h
@@ -20,8 +20,8 @@ public:
 	void Recipient_page(int, vector <Recipient>&, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&);
 	void Update_Data(int, vector <Recipient>&);
 	void Delete_Account(int, vector <Recipient>&);
-	void Search_for_Blood(queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&);
+	void Search_for_Blood(vector <Recipient>&, int, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&);
 	void display_all_blood_data();
-	void Request_Blood();
+	void Request_Blood(string, queue<Blood>&, queue<Blood>&, queue<Blood>&, queue<Blood>&);
 	void Recipient_Registeration_Page(vector <Recipient>& recipientsList, queue<Blood>& dataA, queue<Blood>& dataB, queue<Blood>& dataO, queue<Blood>& dataAB, int&);
 };
