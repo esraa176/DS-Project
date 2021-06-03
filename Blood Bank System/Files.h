7e64117f313c240dbd53d20b68743833311124d1
esraa176 <72940480+esraa176@users.noreply.github.com>
2021-06-04 01:38:31 +0200

    Update Files.h

--- Blood Bank System/Files.h
+++ Blood Bank System/Files.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <fstream>
 #include <iostream>
-#include<vector>
-#include<queue>
+#include <vector>
+#include <queue>
 #include "Admin.h"
 #include "Donor.h"
 #include "Recipient.h"
@@ -11,7 +11,11 @@ using namespace std;
 class Files
 {
 public:
-	void Update_Files(vector <Admin> &, vector <Recipient> &, vector <Donor> &, queue <Blood> &, queue <Blood> &, queue <Blood> &, queue <Blood>&, queue <int>& , int&, int&, int &);
-	void Intialize_Vectors_Queues(vector <Admin> &, vector <Recipient> &, vector <Donor> &, queue <Blood> &, queue <Blood> &, queue <Blood> &, queue <Blood>&, queue <int>& , int&, int&, int &);
+	void Update_Files(vector <Admin> &, vector <Recipient> &, vector <Donor> &, queue <Blood> &, queue <Blood> &, queue <Blood> &, queue <Blood>&, queue <int>&, int&, int&, int &);
+	void Intialize_Vectors_Queues(vector <Admin> &, vector <Recipient> &, vector <Donor> &, queue <Blood> &, queue <Blood> &, queue <Blood> &, queue <Blood>&, queue <int>&, int&, int&, int &);
+	void adminUpdate(vector <Admin> &,int&);
+	void resipientUpdate(vector <Recipient> &, int&);
+	void donorUpdate(vector <Donor> &, int&);
+	void bloodUpdate(queue <Blood> &, queue <Blood> &, queue <Blood> &, queue <Blood>&);
+	void requestsUpdate(queue <int>&);
 };
-
