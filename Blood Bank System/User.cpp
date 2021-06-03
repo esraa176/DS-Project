2fb544e6cc360c6814f986b57206cead544f4ad5
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-06-03 22:17:13 +0200

    Update User.cpp

--- Blood Bank System/User.cpp
+++ Blood Bank System/User.cpp
@@ -8,20 +8,19 @@ User::User()
 
 }
 
-User::User(int id,string name, int age, char gender, string mail, string pass)
+User::User( string name, int age, char gender, string mail, string pass)
 {
-	ID = id;
 	Name = name;
 	Email = mail;
 	Password = pass;
 	Age = age;
 	Gender = gender;
 }
-void User:: Update_Data(int)
+void User::Update_Data(int)
 {
-	
+
 }
-void User:: Delete_Account()
+void User::Delete_Account()
 {
-	
+
 }
