32b109d3006b426e7c73afafd0c18fde6d1a55d5
RawanSadek <76558250+RawanSadek@users.noreply.github.com>
2021-06-04 13:22:18 +0200

    Update Blood.cpp

--- Blood Bank System/Blood.cpp
+++ Blood Bank System/Blood.cpp
@@ -1,8 +1,28 @@
 #include "Blood.h"
+
 Blood::Blood()
 {
-	expiry.tm_mday = received.tm_mday;
-	expiry.tm_mon = received.tm_mon+1;
+	tm newtime;
+	time_t now = time(0);
+	localtime_s(&newtime, &now);
+	received.tm_mday = newtime.tm_mday;
 
- }
+	received.tm_mon = 1 + newtime.tm_mon;
+
+	received.tm_year = 1900 + newtime.tm_year;
+
+	if (received.tm_mon == 12)
+	{
+		expiry.tm_mday = received.tm_mday;
+		expiry.tm_mon = 1;
+		expiry.tm_year= received.tm_year+1;
+
+	}
+	else
+	{
+		expiry.tm_mday = received.tm_mday;
+		expiry.tm_mon =received.tm_mon+ 1;
+		expiry.tm_year = received.tm_year;
+	}
+}
 
